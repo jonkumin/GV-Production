e9d33929f0efe9ed92eebf9789020eafc0638032
Egor Mesyats <egor.mesyats@moovweb.com>
2020-11-18 18:55:22 +0200

    fix routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,4 @@
-const ONE_HOUR = (60 * 60);
-const ONE_DAY = 60 * 60 * 24;
-import { Router, CustomCacheKey } from '@xdn/core/router';
+import { Router } from '@xdn/core/router';
 import { CACHE_SERVICE_WORKER } from './cache';
 import shoppingFlowRouteHandler from './shoppingFlowRouteHandler';
 
@@ -8,275 +6,141 @@ export default new Router()
   .match('/service-worker.js', ({ serveStatic, cache }) => {
     cache(CACHE_SERVICE_WORKER)
     serveStatic('dist/service-worker.js')
-  })
-
-		.get('/cart.php', ({ proxy, cache }) => {
-	cache({
-		edge:false,
 	})
-	proxy('origin')
-})
-	.match('/customer', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	// uncached routes
+	.get('/cart.php', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-	.match('/wishlist:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/customer', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/giants-club:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/wishlist:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/brands/:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/giants-club:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-	.match('/customer:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/brands/:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/sitemap.xml', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/customer:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/sitemap.xml:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/sitemap.xml', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/robots.txt', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/sitemap.xml:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/robots.txt:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/robots.txt', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-	.match('/customer-support:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/robots.txt:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-		.match('/account.php', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/customer-support:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-		.match('/account.php:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/account.php', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-		.match('/order.html', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/account.php:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-		.match('/order.html:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/order.html', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-	.match('/age-verification', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/order.html:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-	.match('/age-verification:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/age-verification', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/manual-verification', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/age-verification:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/manage/:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/manual-verification', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/admin', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/manage/:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/admin/:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/admin', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/oauth2', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/admin/:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/subscribe', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/oauth2', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/subscribe:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/subscribe', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/brands:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/subscribe:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/oauth2/:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/brands:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/login-as-customer', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/oauth2/:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/login-as-customer:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
+	.match('/login-as-customer', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/manual-verification:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/login-as-customer:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/order-verification', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/manual-verification:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/order-verification:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/order-verification', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/vendors', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/order-verification:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/vendors:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/vendors', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/contact-us', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/vendors:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-.match('/contact-us:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/contact-us', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-
-		.match('/finishorder.php', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/contact-us:splat*', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-		.match('/finishorder.php:splat*', ({ proxy, cache }) => {
-	cache({
-		edge:false,
+	.match('/finishorder.php', ({ proxy }) => {
+		proxy('origin')
 	})
-	proxy('origin')
-})
-    .get('/api/storefront/:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-.get('/login.php', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-.match('/login.php:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-
-.get('/checkout.php', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-.match('/checkout.php:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-.match('/verification-in-progress', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-.match('/verification-in-progress:splat*', ({ proxy, cache }) => {
-	cache({
-		edge: false,
-		})
-	proxy('origin')
-})
-
+	.match('/finishorder.php:splat*', ({ proxy }) => {
+		proxy('origin')
+	})
+  .get('/api/storefront/:splat*', ({ proxy }) => {
+		proxy('origin')
+	})
+	.get('/login.php', ({ proxy }) => {
+		proxy('origin')
+	})
+	.match('/login.php:splat*', ({ proxy }) => {
+		proxy('origin')
+	})
+	.get('/checkout.php', ({ proxy }) => {
+		proxy('origin')
+	})
+	.match('/checkout.php:splat*', ({ proxy }) => {
+		proxy('origin')
+	})
+	.match('/verification-in-progress', ({ proxy }) => {
+		proxy('origin')
+	})
+	.match('/verification-in-progress:splat*', ({ proxy }) => {
+		proxy('origin')
+	})
+	// cached files/pages
 	.match('/main.js', ({ serveStatic, cache }) => {
     cache({
       browser: {
@@ -288,9 +152,7 @@ export default new Router()
     })
     serveStatic('dist/browser.js')
   })
-.match('/', shoppingFlowRouteHandler)
-
-.match('cdn11.bigcommerce.com/:splat*', shoppingFlowRouteHandler)
-
-.match('/:splat*', shoppingFlowRouteHandler)
+	.match('/', shoppingFlowRouteHandler)
+	.match('cdn11.bigcommerce.com/:splat*', shoppingFlowRouteHandler)
+	.match('/:splat*', shoppingFlowRouteHandler)
   
